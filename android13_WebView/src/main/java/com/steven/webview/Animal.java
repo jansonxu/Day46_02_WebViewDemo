8ac83638356760e1f8abcdbc3531ddb8aede5f86
jansonxu <jansonxu@sina.cn>
2016-11-17 15:19:08 +0800

    俺添加了注释哦！。。。

--- android13_WebView/src/main/java/com/steven/webview/Animal.java
+++ android13_WebView/src/main/java/com/steven/webview/Animal.java
@@ -4,10 +4,10 @@ package com.steven.webview;
  * Description： xxx<br/>
  * Copyright (c)   2016,  Jansonxu <br/>
  * This program is protected by copyright laws <br/>
- * Date: 2016年11月17  15:06
+ * Date: 2016年11月17  15:18
  *
  * @author 徐文波
  * @version : 1.0
  */
-public class Student {
+public class Animal {
 }
